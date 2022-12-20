a9aef86dba698fd6c44d1c01aaf9da8b4063fc91
ncullen93 <nicholas.cullen@med.lu.se>
2022-12-20 14:45:43 -0500

    added external + internal data

---
+++ R/data.R
@@ -0,0 +1,35 @@
+#' Example pgx object
+#'
+#' This is an example pgx object already created. It comes from
+#' Geiger2016-arginine.
+#'
+#' @format ## `GEIGER_PGX`
+#' A pgx object
+#' \describe{
+#'   \item{name}{PGX name}
+#'   \item{description}{PGX description}
+#'   \item{date}{PGX data created}
+#'   ...
+#' }
+#' @source Geiger2016
+"GEIGER_PGX"
+
+
+#' Get path to omp example dataset(s)
+#'
+#' `playbase` comes bundled with a number of sample files in its `inst/extdata`
+#' directory. This function make them easy to access. This function was
+#' taken from tidyverse/readr.
+#'
+#' @param file Name of file. If `NULL`, the example files will be listed.
+#' @export
+#' @examples
+#' playbase_example()
+#' playbase_example("counts.csv")
+playbase_example <- function(file = NULL) {
+    if (is.null(file)) {
+        dir(system.file("extdata", package = "playbase"))
+    } else {
+        system.file("extdata", file, package = "playbase", mustWork = TRUE)
+    }
+}
