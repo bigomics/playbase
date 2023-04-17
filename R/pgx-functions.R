201ea5ec6fd629bc1bcafd55a474eaec63924fa4
Mauro Masiero <mauromiguelm@hotmail.com>
2023-04-17 17:14:27 +0200

    remove playbase namespace from internal functions

--- R/pgx-functions.R
+++ R/pgx-functions.R
@@ -1654,7 +1654,7 @@ cor.pvalue <- function(x,n) pnorm(-abs(x/((1-x**2)/(n-2))**0.5))
 #' @export
 getGSETS <- function(gs, lib.dir) {
   GENE.SYMBOL = unlist(as.list(org.Hs.eg.db::org.Hs.egSYMBOL))
-  FAMILIES <- playbase::pgx.getGeneFamilies(GENE.SYMBOL, FILES=lib.dir, min.size=10, max.size=9999)
+  FAMILIES <- pgx.getGeneFamilies(GENE.SYMBOL, FILES=lib.dir, min.size=10, max.size=9999)
   fam.file <- file.path(lib.dir,"custom-families.gmt")
   if(file.exists(fam.file)) {
       custom.gmt = read.gmt(file.path(lib.dir,"custom-families.gmt"),add.source=TRUE)
@@ -1665,7 +1665,7 @@ getGSETS <- function(gs, lib.dir) {
   f1 <- FAMILIES
   names(f1) <- paste0("FAMILY:",names(f1))
   names(f1) <- sub("FAMILY:<all>","<all>",names(f1))
-  GSETS <- c(playbase::GSETS,f1)
+  GSETS <- c(GSETS,f1)
 
   GSET.GENES <- sort(unique(unlist(GSETS)))  ## slow...
   iGSETS <- parallel::mclapply(GSETS, function(a) match(a,GSET.GENES))  ## slow...
