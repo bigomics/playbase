a1a96a9327038cbf9377d24cbf1f092790e67a99
ncullen93 <nicholas.cullen@med.lu.se>
2023-04-24 10:52:37 +0200

    add computePGX test

--- tests/testthat/test-pgx_core.R
+++ tests/testthat/test-pgx_core.R
@@ -1,13 +1,19 @@
 if (!identical(Sys.getenv("NOT_CRAN"), "true")) return()
 
 # this takes too long
-test_that("create_pgx and compute example works", {
+test_that("createPGX and computePGX works on default example", {
   counts <- read_counts(example_file("counts.csv"))
   samples <- read_samples(example_file("samples.csv"))
   contrasts <- read_contrasts(example_file("contrasts.csv"))
 
+  # create pgx from default counts, samples, contrasts
   testthat::expect_no_error(
-    my_pgx2 <- pgx.createPGX(counts, samples, contrasts)
+    my_pgx <- pgx.createPGX(counts, samples, contrasts)
+  )
+
+  # compute pgx from the created pgx with default methods
+  testthat::expect_no_error(
+    my_pgx <- pgx.computePGX(my_pgx)
   )
 
 })
