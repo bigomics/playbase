7f66b33c15f681c43c8c751965d0dbdcad2c8cbc
Mauro Masiero <mauromiguelm@hotmail.com>
2023-05-23 11:40:53 +0200

    clean functions

--- R/compute2-genesets.R
+++ R/compute2-genesets.R
@@ -94,7 +94,6 @@ compute_testGenesets <- function(pgx,
     ## filter gene sets on size
     cat("Filtering gene sets on size...\n")
     gmt.size = Matrix::colSums(G!=0)
-    summary(gmt.size)
     size.ok <- (gmt.size >= 15 & gmt.size <= 400 )
     G <- G[, which(size.ok)]
     dim(G)
@@ -112,7 +111,6 @@ compute_testGenesets <- function(pgx,
     } else {
         data.type <- gsub("\\[|\\].*","",rownames(pgx$counts))
         jj <- which(data.type %in% c("gx","mrna"))
-        length(jj)
         if(length(jj)==0) {
             stop("FATAL. could not find gx/mrna values.")
         }
@@ -172,10 +170,7 @@ compute_testGenesets <- function(pgx,
         must.include <- "hallmark|kegg|^go|^celltype|^pathway"
         jj = unique( c(jj, grep(must.include,colnames(G),ignore.case=TRUE)))
         jj = jj[order(colnames(G)[jj])]
-        length(jj)
         G = G[,jj,drop=FALSE]
-        remove(gsetX)
-        remove(gsetX.bygroup)
     }
     dim(G)
 
@@ -297,7 +292,6 @@ clean_gmt <- function(gmt.all, gmt.db){
     }
     names(gmt.all) <- NULL
     gmt.all <- unlist(gmt.all,recursive=FALSE, use.names=TRUE)
-    length(gmt.all)
 
     ## get rid of trailing numeric values
     gmt.all <-  mclapply(gmt.all, function(x) gsub("[,].*","",x), mc.cores=1)
@@ -326,58 +320,38 @@ createSparseGenesetMatrix <- function(
     max.geneset.size = 500) {
     
     ## ----------- Get all official gene symbols
-    library(org.Hs.eg.db)
-    ##library(org.Mm.eg.db)
-    symbol <- as.list(org.Hs.egSYMBOL)
+    symbol <- as.list(org.Hs.eg.db::org.Hs.egSYMBOL)
     known.symbols <- sort(unique(unlist(symbol)))
-    ##mm.symbol <- as.list( org.Mm.egSYMBOL)
-    ##mm.symbol <- sort(unique(unlist(mm.symbol)))
-    ##tail(mm.symbol,100)
-    ##known.symbols <- unique(c(known.symbols, toupper(mm.symbol)))
-
+    
     ##------------- filter by size
     gmt.size <- sapply(gmt.all,length)
-    summary(gmt.size)
 
-    # gmt.all <- gmt.all[which(gmt.size >= 15 & gmt.size <= 200)]
-    gmt.all <- gmt.all[which(gmt.size >= 15 & gmt.size <= 1000)] #legacy
-
-    length(gmt.all)
+    gmt.all <- gmt.all[which(gmt.size >= 15 & gmt.size <= 1000)]
     
     ## ------------- filter genes by minimum frequency and chrom
-    ##symbol = unlist(as.list(org.Hs.egSYMBOL))
-    ##refseq = unlist(as.list(org.Hs.egREFSEQ))
     genes.table <- table(unlist(gmt.all))
-    summary(as.integer(genes.table))
-    length(gmt.all)
+    
     #genes <- names(which( genes.table >= 10 & genes.table <= 1000  ))
     genes <- names(which( genes.table >= 10 ))
     genes <- genes[grep("^LOC|RIK$",genes,invert=TRUE)]
     genes <- intersect(genes, known.symbols)
     annot <- playbase::ngs.getGeneAnnotation(genes)
     genes <- genes[ !is.na(annot$chr) ]
-    length(genes)
-
+    
     ## Filter genesets with permitted genes (official and min.sharing)
     gmt.all <- mclapply(gmt.all, function(s) intersect(s,genes))
     gmt.size <- sapply(gmt.all,length)
-    summary(gmt.size)
     # gmt.all <- gmt.all[which(gmt.size >= 15 & gmt.size <= 200)]
     gmt.all <- gmt.all[which(gmt.size >= min.geneset.size & gmt.size <= min.geneset.size)] #legacy
-    length(gmt.all)
-
     ## build huge sparsematrix gene x genesets
     genes <- sort(genes)
     idx.j <- mclapply(gmt.all[], function(s) match(s,genes))
     idx.i <- lapply(1:length(gmt.all), function(i) rep(i,length(idx.j[[i]])))
     ii <- unlist(idx.i)
     jj <- unlist(idx.j)
-    length(ii)
-    length(jj)
+    
     G <- Matrix::sparseMatrix( i=ii, j=jj, x=rep(1,length(ii)),
                       dims = c(length(gmt.all), length(genes)) )
-    dim(G)
-    
     colnames(G) = genes
     rownames(G) = names(gmt.all)
 
