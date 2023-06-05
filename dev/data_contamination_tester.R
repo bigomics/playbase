b3774eb2ebc228e81f1a940a1098dfcefb99933d
Mauro Masiero <mauromiguelm@hotmail.com>
2023-06-05 14:54:19 +0200

    search for dirs with relevant files

--- dev/data_contamination_tester.R
+++ dev/data_contamination_tester.R
@@ -3,9 +3,19 @@
 ## Copyright (c) 2018-2023 BigOmics Analytics SA. All rights reserved.
 ##
 
-filepath <-dir(filepath, recursive = TRUE, full.names = TRUE)
+filepaths <- ?list.dirs("data-test/", recursive = TRUE)
+
+file.path(filepath[1])
+
+lapply(filepaths,function(x){
+    any(grepl("\\..+", dir(x), ignore.case = TRUE))
+}) -> directories_with_files
+
+filepath <- filepaths[unlist(directories_with_files)]
 
 # same upload as opg
+
+
 
 samples <- playbase::read.as_matrix(fn2)
 counts <- playbase::read.as_matrix(fn2)
