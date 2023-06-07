ed1387bde883faba90dba9e04cf0b18e10626d1c
Mauro Masiero <mauromiguelm@hotmail.com>
2023-06-07 13:25:56 +0200

    identify the right contrast

--- dev/data_contamination_tester.R
+++ dev/data_contamination_tester.R
@@ -11,20 +11,25 @@ filesname <- dir(filespath)
 lapply(filesname, function(x){
   tmp <- playbase::pgx.load(file.path(filespath,x))
 }) -> pgx_files
-
 # check special characters
 
 names(pgx_files) <- filesname
 
-example_data_truth <- pgx_files[["example_data.pgx"]]
+example_data_truth <- pgx_files[[ "data-test__example_data.pgx"]]
 
 example_gx <- example_data_truth$gx.meta$meta$"act_vs_notact"$"avg.1"
 
 example_gset <- as.numeric(example_data_truth$gset.meta$meta$"act_vs_notact"[,"fc"])
 
+contrast_to_test <- "act_vs_notact"
 
 lapply(pgx_files, function(x){
-  #x = pgx_files[[1]]
+  #x = pgx_files[[10]]
+  contrast_default <- contrast_to_test
+
+  if(!contrast_to_test %in% names(x$gx.meta$meta)) {
+    contrast_default <- grep(contrast_to_test, names(x$gx.meta$meta), value = TRUE, ignore.case = TRUE)
+  }
   gx <- cor(x$gx.meta$meta$"act_vs_notact"$"avg.1", example_gx)
   gset <- cor(as.numeric(x$gset.meta$meta$"act_vs_notact"[,"fc"]), example_gset)
 
