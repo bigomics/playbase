b15e0ea7995e6f5955e7020b7475cf40b5829117
ncullen93 <nicholas.cullen@med.lu.se>
2023-07-04 17:57:40 +0200

    remove DT as dependency

--- R/pgx-functions.R
+++ R/pgx-functions.R
@@ -1436,12 +1436,7 @@ psort <- function(x, p.col = NULL) {
   x[order(x[, j]), ]
 }
 
-#' @export
-color_from_middle <- function(data, color1, color2) {
-  ## from https://stackoverflow.com/questions/33521828/
-  max_val <- max(abs(data), na.rm = TRUE)
-  DT::JS(sprintf("isNaN(parseFloat(value)) || value < 0 ? 'linear-gradient(90deg, transparent, transparent ' + (50 + value/%s * 50) + '%%, %s ' + (50 + value/%s * 50) + '%%,%s  50%%,transparent 50%%)': 'linear-gradient(90deg, transparent, transparent 50%%, %s 50%%, %s ' + (50 + value/%s * 50) + '%%, transparent ' + (50 + value/%s * 50) + '%%)'", max_val, color1, max_val, color1, color2, color2, max_val, max_val))
-}
+
 
 #' @export
 tidy.dataframe <- function(Y) {
