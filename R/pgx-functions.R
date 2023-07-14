28beaf321cba6f4d6b4e2006d94bea1c3893e611
ivokwee <ivokwee@users.noreply.github.com>
2023-07-14 08:49:08 +0000

    Style code (GHA)

--- R/pgx-functions.R
+++ R/pgx-functions.R
@@ -506,12 +506,12 @@ read.as_matrix <- function(file) {
     rownames(x) <- x0[[1]][sel]
   }
   ## drop any rows with 100% missing value (sometimes added by not-so-Excel...)
-  zero.row <- which(rowSums(is.na(x))==ncol(x))
+  zero.row <- which(rowSums(is.na(x)) == ncol(x))
   if (length(zero.row)) {
     x <- x[-zero.row, , drop = FALSE]
   }
   ## drop any 100% missing columns (sometimes added by not-so-Excel...)
-  zero.col <- which(colSums(is.na(x))==nrow(x))
+  zero.col <- which(colSums(is.na(x)) == nrow(x))
   if (length(zero.col)) {
     x <- x[, -zero.col, drop = FALSE]
   }
