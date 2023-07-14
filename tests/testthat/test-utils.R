b35d43a1e744ed1c99a9c8aa03ccf722c474a2ad
Cano-Muniz, Santiago <33370667+phisanti@users.noreply.github.com>
2023-07-14 13:46:23 +0100

    Add test for mem.proc (utils)

---
+++ tests/testthat/test-utils.R
@@ -0,0 +1,19 @@
+#' Test for mem.proc
+test_that("mem.proc returns default value on non-Linux systems", {
+  # Skip test if running on Linux
+  if (Sys.info()["sysname"] == "Linux") {
+    skip("Test not applicable on Linux systems")
+  }
+  
+  # Test function
+  result <- mem.proc()
+  
+  # Check result
+  expect_equal(result, "[? MB]")
+})
+ 
+#' Test for info
+#' 
+#' 
+
+#' Test for dbg
