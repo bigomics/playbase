81c50ab51c640eabd8864f4693a4b726874a0433
Ivo Kwee <ivo.kwee@gmail.com>
2023-07-27 01:43:34 +0200

    force comma separator (fread auto bug), reduce pca dim

--- R/pgx-functions.R
+++ R/pgx-functions.R
@@ -511,12 +511,12 @@ read.as_matrix <- function(file) {
 
 
 #' @export
-fread.csv <- function(file, check.names = FALSE, row.names = 1, sep = "auto",
+fread.csv <- function(file, check.names = FALSE, row.names = 1, sep = ",",
                       stringsAsFactors = FALSE, header = TRUE, asMatrix = TRUE) {
   df <- data.table::fread(
     file = file, check.names = check.names, header = header,
     sep = sep, fill = TRUE
-  )
+  )  
   x <- data.frame(df[, 2:ncol(df)],
     stringsAsFactors = stringsAsFactors,
     check.names = check.names
