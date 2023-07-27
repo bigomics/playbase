9ebda816c77b6af47c51b58c51d932b96d115c82
Cano-Muniz, Santiago <33370667+phisanti@users.noreply.github.com>
2023-07-27 10:25:39 +0100

    Add unit test pgx.getFamilies

--- tests/testthat/test-pgx-api.R
+++ tests/testthat/test-pgx-api.R
@@ -1,3 +1,18 @@
-#' Test for pgx.getModelGroups
-#' TODO
-#' 
+#' Test for pgx.getFamilies
+d <- get_mini_example_data()
+pgx <- playbase::pgx.createPGX(counts = d$counts, 
+  samples = d$samples, 
+  contrast = d$contrast)
+test_that("pgx.getFamilies returns expected families", {
+
+  # Test with default parameters
+  families <- pgx.getFamilies(pgx)
+  
+  # Expect 2 families returned
+  expect_equal(length(families), 1)
+  
+  # Expect FAMILY1 and FAMILY3 returned
+  expect_equal(families,"<all>")
+  
+
+})
