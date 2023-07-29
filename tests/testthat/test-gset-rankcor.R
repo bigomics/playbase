fc9bf0324e99d30e7b254de8878e1ce15c0e0077
Cano-Muniz, Santiago <33370667+phisanti@users.noreply.github.com>
2023-07-29 16:54:26 +0100

    add unit test gset-rankcor

---
+++ tests/testthat/test-gset-rankcor.R
@@ -0,0 +1,36 @@
+#' Test for gset.rankcor
+set.seed(1746)
+ranks <- sample(1:10000, 1000, replace = TRUE)
+names(ranks) <- replicate(1000, paste(sample(LETTERS, 4, replace = TRUE), collapse = ""))
+genesets <- matrix(rnorm(1000 * 20), ncol = 20)
+rownames(genesets) <- names(ranks)
+
+result <- gset.rankcor(ranks, genesets)
+
+test_that("gset.rankcor returns a list", {
+
+  expect_type(result, "list")
+})
+
+test_that("gset.rankcor matrix has correct dimensions", {
+
+  expect_equal(dim(result$rho), c(20, 1))
+  expect_equal(dim(result$rho), c(20, 1))
+
+})
+
+test_that("gset.rankcor constant rho", {
+
+  # Mean correlation between random val should be 0
+  expect_equal(mean(result$rho), 0, tolerance = 1)
+
+})
+
+#' Test for gx.nnmcorrect.SAVE
+test_that("gx.nnmcorrect.SAVE returns a matrix of proper dim", {
+  xcorr <- gx.nnmcorrect.SAVE(x, y)
+
+  expect_type(xcorr, "double")
+  expect_equal(dim(xcorr), dim(x))
+})
+
