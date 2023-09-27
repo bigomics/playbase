6dbaa3db263fc295f9a9b0bbccccbafdc320c3ed
Mauro Masiero <mauromiguelm@hotmail.com>
2023-09-27 11:16:48 +0200

    create table from source

--- dev/species-table-dictionary.R
+++ dev/species-table-dictionary.R
@@ -2,8 +2,16 @@
 ## This file is part of the Omics Playground project.
 ## Copyright (c) 2018-2023 BigOmics Analytics SA. All rights reserved.
 ##
+library(biomaRt)
+library(data.table)
+ensembl <- useMart("ensembl")
+species <- listDatasets(ensembl)
+species <- data.table(species)
+species[, species_name := sub("\\s*\\(.*\\)", "", description)]
+species[, species_name := sub(" genes", "", species_name)]
 
+write.csv(species, "dev/SPECIES_DICTIONARY.csv", row.names = FALSE, quote = FALSE)
 
-SPECIES_TABLE <- read.csv("dev/SPECIES_DICTIONARY.csv", header = TRUE, stringsAsFactors = FALSE, check.names = FALSE, fileEncoding="UTF-8-BOM")
+SPECIES_TABLE = species
 
 usethis::use_data(SPECIES_TABLE, overwrite = TRUE)
