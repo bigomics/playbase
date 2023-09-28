46c5fe59c7e605643d7902f8d6c94f4a5c77ecb9
Cano-Muniz, Santiago <33370667+phisanti@users.noreply.github.com>
2023-09-28 20:50:08 +0100

    get_mini_example_data option separate data

--- R/utils.R
+++ R/utils.R
@@ -30,6 +30,8 @@ dbg <- function(...) info(..., type = "DBUG")
 #' Returns a mini example dataset with sample information,
 #' gene expression counts, and contrasts.
 #'
+#' @param item Select which elements to return c("counts", "samples", "contrats", "all")
+#' by default it returns a list with all the data elements to build a pgx object.
 #' @return List containing:
 #' \itemize{
 #'   \item samples - data.frame with sample info
@@ -40,7 +42,16 @@ dbg <- function(...) info(..., type = "DBUG")
 #' @examples
 #' d <- get_mini_example_data()
 #' @export
-get_mini_example_data <- function() {
+get_mini_example_data <- function(item = "all") {
+
+  if (!item %in% c("counts", "samples", "contrats", "all")) {
+    stop("Item not matched. Please use: 'counts', 'samples', 'contrats', or 'all'")
+  }
+  if (length(item) > 1L) {
+    stop("Call only one item at a time. For multiple items call, use 'all'")
+  }
+
+  # Prepare input data
   counts <- playbase::COUNTS
   samples <- playbase::SAMPLES
   contrast <- playbase::CONTRASTS
@@ -51,8 +62,15 @@ get_mini_example_data <- function() {
   mini_counts <- counts[n_genes, c(1:3, 8:9, 11:12)]
   mini_samples <- samples[colnames(mini_counts), ]
   mini_contrast <- contrast[1:3, 1:3]
+  mini_data <- list(counts = mini_counts,
+                    samples = mini_samples,
+                    contrast = mini_contrast)
 
-  mini_data <- list(counts = mini_counts, samples = mini_samples, contrast = mini_contrast)
+  if (item == "all") {
 
-  return(mini_data)
+    return(mini_data)
+
+  } else {
+    return(mini_data[[item]])
+  }
 }
