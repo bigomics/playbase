34ef5cf2c9340a4756ff643f6b5779f02735f415
Mauro Masiero <mauromiguelm@hotmail.com>
2023-09-28 14:50:20 +0200

    add species from ensembl genome

--- dev/species-table-dictionary.R
+++ dev/species-table-dictionary.R
@@ -4,11 +4,46 @@
 ##
 library(biomaRt)
 library(data.table)
+
+# get species from main ensembl vertebrate
+
 ensembl <- useMart("ensembl")
 species <- listDatasets(ensembl)
 species <- data.table(species)
 species[, species_name := sub("\\s*\\(.*\\)", "", description)]
 species[, species_name := sub(" genes", "", species_name)]
+
+# add col ds to species table
+species$ds <- "ensembl"
+
+
+# get other taxonomies from ensenbl genome
+
+genomes <- listEnsemblGenomes()
+
+ds_genomes <- lapply(genomes$biomart, function(x) {
+    #x = genomes$biomart[1]
+    ensembl <- useEnsemblGenomes(biomart = x)
+    ds <- listDatasets(ensembl)
+    ds$ds <- x
+    return(ds)
+})
+
+ds_genomes <- do.call("rbind", ds_genomes)
+
+ds_genomes$species_name <- sub("\\s*\\(.*", "", ds_genomes$description)
+# remove " genes" fro the string
+ds_genomes$species_name <- sub(" genes", "", ds_genomes$species_name)
+# remoe Short Variants from the string
+ds_genomes$species_name <- sub(" Short Variants", "", ds_genomes$species_name)
+# remove Structural Variants from the string
+ds_genomes$species_name <- sub(" Structural Variants", "", ds_genomes$species_name)
+# if duplicate, keep only first entry
+ds_genomes <- ds_genomes[!duplicated(ds_genomes$species_name), ]
+
+# merge two tables
+
+species <- rbind(species, ds_genomes)
 
 # order table by Human, Mouse and Rat to appear first in species_name
 species <- species[order(species_name %in% c("Human", "Mouse", "Rat"), -as.character(species_name))]
@@ -21,3 +56,5 @@ write.csv(species, "dev/SPECIES_DICTIONARY.csv", row.names = FALSE, quote = FALS
 SPECIES_TABLE = species
 
 usethis::use_data(SPECIES_TABLE, overwrite = TRUE)
+
+
