aa2846c8ee7fb926a77dcfabda6f28ace80d4458
phisanti <tisalon@outlook.com>
2023-10-03 10:28:10 +0100

    probe_detect hgcn_symbol -> external_gene_name

--- R/pgx-ensembl.R
+++ R/pgx-ensembl.R
@@ -76,7 +76,7 @@ detect_probe <- function(probes, mart = NULL, verbose = TRUE){
   probe_types_to_check <- c("ensembl_gene_id",
                             "ensembl_transcript_id",
                             "ensembl_transcript_id_version",
-                            "hgnc_symbol",
+                            "external_gene_name",
                             "ensembl_gene_id_version",
                             "uniprot_gn_id",
                             "refseq_peptide",
