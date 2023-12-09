6d7c66a6446b4750b7d2ee9d4ed3932938157b9f
ivokwee <ivokwee@users.noreply.github.com>
2023-12-09 01:07:12 +0000

    Style code (GHA)

--- R/pgx-contrasts.R
+++ R/pgx-contrasts.R
@@ -800,7 +800,7 @@ contrasts.convertToLabelMatrix <- function(contrasts, samples) {
     group.col
   }
   has.group.col <- length(group.col) > 0
-  is.group.contrast  <- has.group.col && all(samples[,group.col] %in% rownames(contrasts))
+  is.group.contrast <- has.group.col && all(samples[, group.col] %in% rownames(contrasts))
   is.sample.contrast <- nrow(contrasts) > 0 &&
     all(rownames(contrasts) %in% rownames(samples))
 
@@ -812,7 +812,7 @@ contrasts.convertToLabelMatrix <- function(contrasts, samples) {
     message("[contrasts.convertToLabelMatrix] ERROR: Invalid contrast.")
     return(NULL)
   }
-  
+
   new.contrasts <- contrasts
   ## old1: group-wise -1/0/1 matrix
   if (is.group.contrast && is.numeric.contrast) {
