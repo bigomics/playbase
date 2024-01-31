13a4bb8768e2b97528e0384e0bb808e6e92a5f2c
Mauro Masiero <mauromiguelm@hotmail.com>
2024-01-31 11:30:45 +0100

    save test results in root

--- .github/workflows/unittest/entrypoint.sh
+++ .github/workflows/unittest/entrypoint.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
 
 # Run tests
-R -e "x <- devtools::test(quiet=T)" > test_result.txt
+R -e "x <- devtools::test(quiet=T)" > /test_result.txt
 
 # Read test results from file
-res=$(cat test_result.txt)
+res=$(cat /test_result.txt)
 
 # # return test result as an output (will be deprecated)
 echo ::set-output name=test_result::$res
 
 # return exit status
-failed_tests_count=$(grep -i -c "Failed tests" test_result.txt)
+failed_tests_count=$(grep -i -c "Failed tests" /test_result.txt)
 
 if (( failed_tests_count > 0 )); then
     echo "There are failed tests."
