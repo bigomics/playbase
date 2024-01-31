5e23e9b9444ba90f623825f2662dcfc2f522d0db
Mauro Masiero <mauromiguelm@hotmail.com>
2024-01-31 12:22:01 +0100

    add step to fail workflow is test fails, warning user about failure

--- .github/workflows/unittest/entrypoint.sh
+++ .github/workflows/unittest/entrypoint.sh
@@ -13,8 +13,10 @@ echo ::set-output name=test_result::$res
 failed_tests_count=$(grep -i -c "Failed tests" /test_result.txt)
 
 if (( failed_tests_count > 0 )); then
+    echo ::set-output name=workflow_result::false
     echo "::error:: There are failed tests that failed, please download artifacts for more details."
 else
+    echo ::set-output name=workflow_result::true
     echo "All tests passed."
     exit 0
 fi
