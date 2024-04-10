bed6aab58a704f356da450d321195657d8e9dcf5
Mauro Masiero <mauromiguelm@hotmail.com>
2024-04-10 18:03:07 +0200

    add full pgx computation (all methods)

--- dev/mem_profiling/samples.R
+++ dev/mem_profiling/samples.R
@@ -2,10 +2,11 @@ library(playbase)
 
 options(app.profile = TRUE)
 
-output_dir <- getwd()
+output_dir <- getwd() # root playbase
+output_dir <- paste0(output_dir, "/dev/mem_profiling")
 
-# save in dev/mem_profiling
-output_dir <- paste0(output_dir, "/dev/mem_profiling/")
+# read params of a FULL settings computation
+params <- readRDS(file.path(output_dir,"params.RData"))
 
 duplicate_samples <- function(n) {
     samples <- playbase::SAMPLES
@@ -47,14 +48,47 @@ iterations = seq(1,101,10) * 18
 
 for(i in iterations) {
     input <- duplicate_samples(n=i)
+    
     pgx <- playbase::pgx.createPGX(
-        samples = input$samples,
+        organism = params$organism,
         counts = input$counts,
-        contrasts = input$contrasts
+        samples = input$samples,
+        contrasts = input$contrasts,
+        name = params$name,
+        datatype = params$datatype,
+        description = params$description,
+        creator = params$creator,
+        X = NULL,
+        batch.correct = params$batch.correct,
+        normalize = params$normalize,
+        prune.samples = params$prune.samples,
+        filter.genes = params$filter.genes,
+        only.known = params$only.known,
+        only.proteincoding = params$only.proteincoding,
+        only.hugo = params$only.hugo,
+        convert.hugo = params$convert.hugo,
+        custom.geneset = params$custom.geneset,
+        max.genesets = params$max.genesets,
+        annot_table = params$annot_table
     )
-    write.csv(summaryRprof(memory = "tseries"), paste(output_dir,"sample_mem_create_tseries_", i, ".csv", sep=""))
+    
+    write.csv(summaryRprof(memory = "tseries"), paste(output_dir,"/samples_output/","sample_mem_create_tseries_", i, ".csv", sep=""))
+    
     pgx <- playbase::pgx.computePGX(
-        pgx = pgx
+        pgx = pgx,
+        max.genes = params$max.genes,
+        gx.methods = params$gx.methods,
+        gset.methods = params$gset.methods,
+        extra.methods = params$extra.methods,
+        use.design = params$use.design,        ## no.design+prune are combined
+        prune.samples = params$prune.samples,  ##
+        do.clustergenes = params$do.cluster,
+        do.clustergenesets = params$do.cluster,
+        cluster.contrasts = params$cluster.contrasts,
+        pgx.dir = params$pgx.save.folder,
+        libx.dir = params$libx.dir,
+        user_input_dir = output_dir
     )
-    write.csv(summaryRprof(memory = "tseries"), paste(output_dir,"sample_mem_compute_tseries_", i, ".csv", sep=""))
+
+    write.csv(summaryRprof(memory = "tseries"), paste(output_dir,"/samples_output/","sample_mem_compute_tseries_", i, ".csv", sep=""))
 }
