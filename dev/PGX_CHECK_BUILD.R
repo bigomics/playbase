8689802fc56f719c0ab44d70ce50498b61f8e0b1
Mauro Masiero <mauromiguelm@hotmail.com>
2024-05-16 15:46:11 +0200

    update log counts detect message

--- dev/PGX_CHECK_BUILD.R
+++ dev/PGX_CHECK_BUILD.R
@@ -5,9 +5,9 @@
 
 # read PGX_CHECKS.csv
 
-PGX_CHECKS <- read.csv("dev/PGX_CHECKS.csv", header = TRUE, stringsAsFactors = FALSE, check.names = FALSE, fileEncoding="UTF-8-BOM")
+PGX_CHECKS <- read.csv("dev/PGX_CHECKS.csv", header = TRUE, stringsAsFactors = FALSE, check.names = FALSE, fileEncoding = "UTF-8-BOM")
 
 # remove placeholder rows for future checks
-PGX_CHECKS <- PGX_CHECKS[!PGX_CHECKS$message == "",]
+PGX_CHECKS <- PGX_CHECKS[!PGX_CHECKS$message == "", ]
 
 usethis::use_data(PGX_CHECKS, overwrite = TRUE)
