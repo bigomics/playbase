7fd87e9da20e1c898b4b0491817bc820d61ee16c
Xavier Escribà Montagut <xavier.escriba.montagut@gmail.com>
2024-06-22 00:20:17 +0200

    back to full fgsea, no block

--- R/pgx-signature.R
+++ R/pgx-signature.R
@@ -145,27 +145,27 @@ pgx.correlateSignatureH5 <- function(fc, h5.file, nsig = 100, ntop = 200, nperm 
   ## Test signatures using fGSEA (this is pretty fast. amazing.)
   ## ------------------------------------------------------------
   ## combine up/down into one (unsigned GSEA test)
-  split_list <- function(input_list, chunk_size) {
-    split(input_list, ceiling(seq_along(input_list)/chunk_size))
-  }
+  # split_list <- function(input_list, chunk_size) {
+  #   split(input_list, ceiling(seq_along(input_list)/chunk_size))
+  # }
   system.time({
     gmt <- rbind(sig100.up, sig100.dn)
     gmt <- unlist(apply(gmt, 2, list), recursive = FALSE)
     names(gmt) <- colnames(sig100.up)
     dbg(">>> [pgx-sign] point 5")
     #suppressMessages(suppressWarnings(
-      gmt_blocks <- split_list(gmt, 5000)
-      results_list <- list()
-      for (i in seq_along(gmt_blocks)) {
-        block_result <- fgsea::fgseaMultilevel(gmt_blocks[[i]], abs(fc), nPermSimple = nperm, scoreType = "pos")
-        block_result$ES <- NULL
-        block_result$leadingEdge <- NULL
-        block_result$pval <- NULL
-        results_list[[i]] <- block_result
-      }
-      res <- do.call(rbind, results_list)
+      # gmt_blocks <- split_list(gmt, 5000)
+      # results_list <- list()
+      # for (i in seq_along(gmt_blocks)) {
+      #   block_result <- fgsea::fgseaMultilevel(gmt_blocks[[i]], abs(fc), nPermSimple = nperm, scoreType = "pos")
+      #   block_result$ES <- NULL
+      #   block_result$leadingEdge <- NULL
+      #   block_result$pval <- NULL
+      #   results_list[[i]] <- block_result
+      # }
+      # res <- do.call(rbind, results_list)
 
-      # res <- fgsea::fgseaMultilevel(gmt, abs(fc), nPermSimple = nperm, scoreType = "pos")
+      res <- fgsea::fgseaMultilevel(gmt, abs(fc), nPermSimple = nperm, scoreType = "pos")
       # https://github.com/ctlab/fgsea/issues/103
     #)) ## really unsigned???
     dbg(">>> [pgx-sign] point 6")
