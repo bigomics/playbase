2519842db107c351e02c3a5f3d96dd941bf0f520
Mauro Masiero <mauromiguelm@hotmail.com>
2024-07-01 10:53:15 +0200

    fix rowsum.default(2^pgx$X, symbol) : incorrect length for 'group'

--- R/pgx-compute.R
+++ R/pgx-compute.R
@@ -413,7 +413,7 @@ pgx.createPGX <- function(counts,
 
     pgx$counts <- pgx$counts[keep, , drop = FALSE]
     if (!is.null(pgx$X)) {
-      keep <- intersect(keep, rownames(pgx$X))
+      keep <- match(rownames(pgx$genes), rownames(pgx$X))
       pgx$X <- pgx$X[keep, , drop = FALSE] ##  NOT ALIGNED???
     }
   }
