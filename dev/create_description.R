88eb80ea2a763350ca1793b4301c13b9123282de
ivokwee <ivo.kwee@gmail.com>
2024-08-03 13:08:10 +0200

    update with URL because github format not working

--- dev/create_description.R
+++ dev/create_description.R
@@ -4,6 +4,11 @@
 ##
 
 
+# This file is supposed to run from the root Playground folder
+if (basename(getwd()) != "playbase") {
+    stop("This file is supposed to run from the root Playbase folder")
+}
+
 ## ---------------------------------------------------------------------
 ## Automatically scan all used packages and install
 ## ---------------------------------------------------------------------
@@ -22,16 +27,29 @@ pkg.remotes <- c(
   "NNLM" = "linxihui/NNLM",
   "KEGG.db" = "ncullen93/KEGG.db",
   "iheatmapr" = "ropensci/iheatmapr",
-  "xbioc" = "renozao/xbioc",
   "SuperCell" = "GfellerLab/SuperCell",
   "Azimuth" = "satijalab/azimuth",
-  "org.Pf.plasmo.db" = "url::https://www.bioconductor.org/packages//2.12/data/annotation/src/contrib/org.Pf.plasmo.db_2.9.0.tar.gz",
+  "org.Pf.plasmo.db" = "url::https://www.bioconductor.org/packages/3.14/data/annotation/src/contrib/org.Pf.plasmo.db_3.14.0.tar.gz",
   "infercnv" = "broadinstitute/infercnv@infercnv-v1.3.3",
   "PCSF" = "bigomics/PCSF",
   "EPIC" = "GfellerLab/EPIC"
 )
 
-pkg.remotes <- pkg.remotes[names(pkg.remotes) %in% pkg.used]
+remotes.url <- c(
+  "PCSF" = "url::https://github.com/bigomics/PCSF/archive/HEAD.zip",
+  "playdata" = "url::https://github.com/bigomics/playdata/archive/HEAD.zip",
+  "infercnv" = "url::https://github.com/broadinstitute/infercnv/archive/HEAD.zip",
+  "EPIC" = "url::https://github.com/GfellerLab/EPIC/archive/HEAD.zip",
+  "SuperCell" = "url::https://github.com/GfellerLab/SuperCell/archive/HEAD.zip",
+  "NNLM" = "url::https://github.com/linxihui/NNLM/archive/HEAD.zip",
+  "iheatmapr" = "url::https://github.com/ropensci/iheatmapr/archive/HEAD.zip",
+  "Azimuth" = "url::https://github.com/satijalab/azimuth/archive/HEAD.zip",
+  "org.Pf.plasmo.db" = "url::https://bioconductor.org/packages/3.14/data/annotation/src/contrib/org.Pf.plasmo.db_3.14.0.tar.gz",
+  "KEGG.db" = "url::https://bioconductor.org/packages/3.11/data/annotation/src/contrib/KEGG.db_3.2.4.tar.gz"
+)
+
+#pkg.remotes <- pkg.remotes[names(pkg.remotes) %in% pkg.used]
+pkg.remotes <- remotes.url[names(remotes.url) %in% pkg.used]
 pkg.imports <- setdiff(pkg.used, names(pkg.remotes))
 
 if(file.exists("DESCRIPTION")) {
