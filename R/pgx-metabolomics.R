fc6d3772ebebfa2d8f76b75b45cb014c36fef074
Mauro Masiero <mauromiguelm@hotmail.com>
2024-08-06 16:59:18 +0200

    add namespace to metabolomics functions; refactor ids to probe

--- R/pgx-metabolomics.R
+++ R/pgx-metabolomics.R
@@ -1,31 +1,31 @@
 # convert IDs to CHEBI using base R functions
 
 #' @export
-convert_probe_to_chebi <- function(ids, probe_type) {
+convert_probe_to_chebi <- function(probes, probe_type) {
     probe_type_dictionary <- playdata::METABOLITE_ANNOTATION
     valid_probe_types <- colnames(probe_type_dictionary)
 
     # for id that are "", set it to na
-    ids[ids == ""] <- NA
+    probes[probes == ""] <- NA
 
     # check that probetype is valid
     probe_type <- match.arg(probe_type, valid_probe_types)
 
     if (probe_type == "ChEBI") {
         # keep only numbers in ids, as chebi ids are numeric
-        chebi_ids <- gsub("[^0-9]", "", ids)
+        chebi_ids <- gsub("[^0-9]", "", probes)
         # check that ChEBI ids are in the dictionary
         chebi_ids <- ifelse(chebi_ids %in% probe_type_dictionary$ChEBI, chebi_ids, NA)
     } else if (probe_type == "HMDB") {
         # use match
-        matches <- match(ids, probe_type_dictionary$HMDB)
+        matches <- match(probes, probe_type_dictionary$HMDB)
         chebi_ids <- probe_type_dictionary[matches, "ChEBI"]
 
         # return NA for unmatched IDs
         return(chebi_ids)
     } else if (probe_type == "KEGG") {
         # use match
-        matches <- match(ids, probe_type_dictionary$KEGG)
+        matches <- match(probes, probe_type_dictionary$KEGG)
         chebi_ids <- probe_type_dictionary[matches, "ChEBI"]
     } else {
         return(NA)
@@ -35,6 +35,31 @@ convert_probe_to_chebi <- function(ids, probe_type) {
 }
 
 #' @export
-pgx.addMetaboliteAnnotation <- function(counts, annotation_table) {
-    return(NULL)
+getMetaboliteAnnotation <- function(probes, probe_type) {
+    # get annotation for probes
+    probes_chebi <- playbase::convert_probe_to_chebi(probes, probe_type)
+
+    metabolite_metadata <- playdata::METABOLITE_METADATA
+
+    match_ids <- match(probes_chebi, metabolite_metadata$ID)
+
+    metabolite_metadata <- metabolite_metadata[match_ids, ]
+
+
+
+    df <- data.frame(
+        feature = probes,
+        symbol = probes_chebi,
+        human_ortholog = probes_chebi,
+        gene_title = metabolite_metadata$NAME,
+        gene_biotype = NA,
+        map = NA,
+        chr = NA,
+        pos = NA,
+        tx_len = NA,
+        source = "MetaboAnalyst",
+        gene_name = probes_chebi
+    )
+
+    return(df)
 }
--- tests/testthat/test-pgx-metabolomics.R
+++ tests/testthat/test-pgx-metabolomics.R
@@ -7,22 +7,22 @@ KEGG <- c("C06958", "C06849", "C18044", "UnrecognizedId", "C16545")
 # Test cases
 test_that("probe_type is correctly validated and converted for metabolomics", {
     # Test valid probe types
-    expect_equal(convert_probe_to_chebi(ids = ChEBI, probe_type = "ChEBI"), c("8874", NA, NA, "49603", "15903", NA, NA))
-    expect_equal(convert_probe_to_chebi(ids = HMDB, probe_type = "HMDB"), c("10093", "135377", "422", NA, "9171", NA, NA))
-    expect_equal(convert_probe_to_chebi(KEGG, "KEGG"), c("101278", "3082", "35420", NA, "63825"))
+    expect_equal(playbase::convert_probe_to_chebi(probes = ChEBI, probe_type = "ChEBI"), c("8874", NA, NA, "49603", "15903", NA, NA))
+    expect_equal(playbase::convert_probe_to_chebi(probes = HMDB, probe_type = "HMDB"), c("10093", "135377", "422", NA, "9171", NA, NA))
+    expect_equal(playbase::convert_probe_to_chebi(probes = KEGG, "KEGG"), c("101278", "3082", "35420", NA, "63825"))
 
     # chebi handles duplicated values
-    expect_equal(convert_probe_to_chebi(c("8874", "8874"), "ChEBI"), c("8874", "8874"))
+    expect_equal(playbase::convert_probe_to_chebi(c("8874", "8874"), "ChEBI"), c("8874", "8874"))
 
     # HMDB handles duplicated values
-    expect_equal(convert_probe_to_chebi(c("HMDB0015464", "HMDB0015464"), "HMDB"), c("10093", "10093"))
+    expect_equal(playbase::convert_probe_to_chebi(c("HMDB0015464", "HMDB0015464"), "HMDB"), c("10093", "10093"))
 
     # KEGG handles duplicated values
-    expect_equal(convert_probe_to_chebi(c("C06958", "C06958"), "KEGG"), c("101278", "101278"))
+    expect_equal(playbase::convert_probe_to_chebi(c("C06958", "C06958"), "KEGG"), c("101278", "101278"))
 
     # Test invalid probe type returns error
-    expect_error(convert_probe_to_chebi(ids, "InvalidType"), "arg should be one of")
+    expect_error(playbase::convert_probe_to_chebi(ChEBI, "InvalidType"))
 
     # Test empty string is converted to NA
-    expect_equal(convert_probe_to_chebi(c("", "HMDB0015464"), "HMDB"), c(NA, "10093"))
+    expect_equal(playbase::convert_probe_to_chebi(c("", "HMDB0015464"), "HMDB"), c(NA, "10093"))
 })
