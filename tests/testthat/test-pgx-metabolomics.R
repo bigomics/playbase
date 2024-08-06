aa8669c04c86767c9f89ba5a945bf14ac4a71cc4
Mauro Masiero <mauromiguelm@hotmail.com>
2024-08-06 12:48:51 +0200

    fix testing issues with chebi and hmdb ids

--- tests/testthat/test-pgx-metabolomics.R
+++ tests/testthat/test-pgx-metabolomics.R
@@ -1,8 +1,6 @@
 # Sample data frame containing all IDs
 
-# devtools::load_all()
-
-ChEBI <- c("15377", "UnrecognizedId", "145677", "15377", "15377", NA)
+ChEBI <- c("8874", "UnrecognizedId", "1445644", "49603", "15903", "", NA)
 HMDB <- c("HMDB0015464", "HMDB0250234", "HMDB0000190", NA, "HMDB00004", "UnrecognizedId")
 KEGG <- c("C00001", "C00002", "C00003", "UnrecognizedId", "C00004")
 
@@ -10,7 +8,7 @@ KEGG <- c("C00001", "C00002", "C00003", "UnrecognizedId", "C00004")
 # Test cases
 test_that("probe_type is correctly validated and converted for metabolomics", {
     # Test valid probe types
-    expect_equal(convert_probe_to_chebi(ChEBI, "ChEBI"), "ADD TEST HERE")
+    expect_equal(convert_probe_to_chebi(ids = ChEBI, probe_type = "ChEBI"), "ADD TEST HERE")
     expect_equal(convert_probe_to_chebi(ids = HMDB, probe_type = "HMDB"), "ADD TEST HERE")
     expect_equal(convert_probe_to_chebi(ChEBI, "ChEBI"), "ADD TEST HERE")
 
