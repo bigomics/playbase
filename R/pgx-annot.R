0a5cc9fcacad4356a5306e3770b0db1f423b9a2e
ivokwee <ivokwee@users.noreply.github.com>
2024-08-20 00:51:38 +0000

    Style code (GHA)

--- R/pgx-annot.R
+++ R/pgx-annot.R
@@ -1168,7 +1168,7 @@ getOrgGeneInfo <- function(organism, gene, feature, datatype, as.link = TRUE) {
   ## create links to PhosphoELM for proten and gene: db of S/T/Y phosphorylation sites
   if (datatype == "proteomics") {
     phosphoELM.link1 <- "<a href='http://phospho.elm.eu.org/byAccession/UNIPROT' target='_blank'>PhosphoELM_protein</a>"
-    feature1 <- sub("[-._].*", "", feature)  ## is it always uniprot???
+    feature1 <- sub("[-._].*", "", feature) ## is it always uniprot???
     phosphoELM.link1 <- sub("UNIPROT", feature1, phosphoELM.link1)
     phosphoELM.link2 <- "<a href='http://phospho.elm.eu.org/bySubstrate/GENE' target='_blank'>PhosphoELM_gene</a>"
     phosphoELM.link2 <- sub("GENE", symbol, phosphoELM.link2)
