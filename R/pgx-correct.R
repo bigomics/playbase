6e19f8af168338a0f354c99746dc75c738e70f82
Ivo Kwee <ivo.kwee@gmail.com>
2024-10-14 17:47:19 +0200

    easier solution

--- R/pgx-correct.R
+++ R/pgx-correct.R
@@ -1752,15 +1752,7 @@ compare_batchcorrection_methods <- function(X, samples, pheno, contrasts,
   t2 <- function(x) as.matrix(t(scale(t(scale(t(x), scale = FALSE)))))
   if (clust.method == "tsne" && nmissing == 0) {
     message("Computing t-SNE clustering...")
-    nb <- max(2, round(min(30, dim(X) / 5)))
-    if (!(3 * nb < nrow(t(X)) - 1)) {
-        if (nrow(t(X)) < 4) {
-            nb <- 0.5
-        } else {
-            ## nb <- floor((nrow(t(X)) - 1) / 3)
-            nb <- nb / 1.5 ## adjust
-        }
-    }
+    nb <- max(1, round(min(30, dim(X) / 5)))
     pos <- lapply(xlist, function(x) {
       Rtsne::Rtsne(t2(x), perplexity = nb, check_duplicates = FALSE)$Y
     })
