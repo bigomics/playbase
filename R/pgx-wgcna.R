2ecd1fe49da90073f9637306137c3c3edafbeba4
zitoa <zito.a@hotmail.com>
2024-10-27 18:34:51 +0100

    Add drop=FALSE as good practice

--- R/pgx-wgcna.R
+++ R/pgx-wgcna.R
@@ -84,7 +84,7 @@ pgx.wgcna <- function(
   nmissing <- sum(is.na(X))
   if (nmissing > 0) {
     message("Found ", nmissing, " missing values in X. Removing prior to WGCNA.")
-    X <- X[complete.cases(X), ]
+    X <- X[complete.cases(X), , drop = FALSE]
   }
   sdx <- matrixStats::rowSds(X, na.rm = TRUE)
   X <- X[sdx > 0.1 * mean(sdx, na.rm = TRUE), ] ## filter low SD
