3a3c6d9567605b7eb6111187a875079fc2ea51f0
Ivo Kwee <ivo.kwee@gmail.com>
2024-12-21 15:52:45 +0100

    revert and altfix for Fix #1291

--- R/pgx-plotting.R
+++ R/pgx-plotting.R
@@ -294,8 +294,12 @@ pgx.dimPlot <- function(X, y, method = c("tsne", "pca", "umap"), nb = NULL, ...)
     }
     if (m == "pca") pos <- try(irlba::irlba(X1, nv = 2, nu = 0)$v)
     if (m == "pacmap") pos <- try(pacmap(t(X1)))
-    if (!"try-errror" %in% class(pos)) {
-      pos <- X1[1:ncol(X), ]
+    if ("try-errror" %in% class(pos)) {
+      pos <- matrix(0, nrow=ncol(X), ncol=2)
+      rownames(pos) <- colnames(X)
+      pgx.scatterPlotXY(pos, var = y, title = m, ...)
+    } else {
+      pos <- pos[1:ncol(X), ]
       rownames(pos) <- colnames(X)
       pgx.scatterPlotXY(pos, var = y, title = m, ...)
     }
