421ca8598c400005312b946c7d3cf3d0c86664ff
zitoa <zito.a@hotmail.com>
2025-02-13 10:17:13 +0100

    use_basilisk=TRUE

--- R/pgx-mofa.R
+++ R/pgx-mofa.R
@@ -239,7 +239,7 @@ mofa.compute <- function(xdata,
     outfile = file.path(tempdir(),"mofa-model.hdf5")
     suppressMessages(suppressWarnings(
       model <- MOFA2::run_mofa(obj, outfile=outfile, save_data=TRUE,
-                               use_basilisk=FALSE)
+                               use_basilisk=TRUE)
     ))
     
     model <- MOFA2::load_model(outfile, remove_inactive_factors = FALSE)
