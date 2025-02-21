f30332be54ad47e9041c8834121e8b6e0d6d3363
zitoa <zito.a@hotmail.com>
2025-02-21 14:31:18 +0100

    add WGCNA reassignThreshold param

--- R/pgx-wgcna.R
+++ R/pgx-wgcna.R
@@ -86,13 +86,15 @@ pgx.wgcna <- function(
 
 
 #' @export
-wgcna.compute <- function(X, samples,
+wgcna.compute <- function(X,
+                          samples,
                           minmodsize = 20,
                           power = 6,
                           cutheight = 0.15,
                           deepsplit = 2,
                           networktype = "signed",
-                          tomtype = "signed",                          
+                          tomtype = "signed",
+                          reassignThreshold = 1e-6,
                           ngenes = 1000,
                           numericlabels = TRUE,
                           prefix = "ME"
@@ -146,7 +148,7 @@ wgcna.compute <- function(X, samples,
     networkType = networktype,
     TOMType = tomtype,
     minModuleSize = minmodsize,
-    # reassignThreshold = 0,
+    reassignThreshold = reassignThreshold,
     mergeCutHeight = cutheight,
     numericLabels = numericlabels, ## numeric or 'color' labels
     deepSplit = deepsplit,
