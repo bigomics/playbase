c941b1a0ff677441b4fe3795617647b557b1b154
Ivo Kwee <ivo.kwee@gmail.com>
2025-11-12 21:35:27 +0100

    edit

--- R/pgx-wgcna.R
+++ R/pgx-wgcna.R
@@ -4993,7 +4993,7 @@ wgcna.describeModules <- function(wgcna, ntop=25, annot=NULL, multi=FALSE,
       if(m %in% names(top$pheno)) pp <- paste( top$pheno[[m]], collapse='; ')
 
       d <- ""
-      if(!is.null(pp)) d <- paste(d, "<b>Top correlated phenotypes:</b> ", pp, "<br><br>")
+      if(!is.null(pp)) d <- paste(d, "<b>Correlated phenotypes:</b> ", pp, "<br><br>")
       d <- paste(d, "<b>Key genes:</b> ", gg, "<br><br>")
       d <- paste(d, "<b>Top enriched gene sets:</b> ", ss, "<br><br>")
       
@@ -5001,7 +5001,6 @@ wgcna.describeModules <- function(wgcna, ntop=25, annot=NULL, multi=FALSE,
     }
     return(desc)
   }
-
   
   prompt <- "Give a short summary of the main overall biological function of the following top enriched genesets belonging to module <MODULE>. Discuss the possible relationship with phenotypes <PHENOTYPES> of this experiment about <EXPERIMENT>. Use maximum one paragraph. Do not use bullet points. \n\nHere is list of enriched gene sets: <GENESETS>\n"
   if(verbose) cat(prompt)
