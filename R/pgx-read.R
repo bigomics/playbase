8eb3553424d149777dec8558043d1ef77cdda5de
zitoa <zito.a@hotmail.com>
2025-12-18 17:10:50 +0100

    read_spectronaut

--- R/pgx-read.R
+++ R/pgx-read.R
@@ -550,6 +550,88 @@ read_h5_counts <- function(h5.file) {
 }
 
 
+#' Read Spectronaut output abundance file and generates abundance matrix.
+#' @param Path to Spectronaut output abundance file.
+#' @return abundance data matrix (features on rows; samples on columns)
+#' @export
+read_spectronaut <- function(file) {
+
+  msg <- function(...) message("[playbase::read_spectronaut] ", ...)
+  
+  counts <- suppressMessages(suppressWarnings(
+    try(playbase::read_counts(file), silent = TRUE)
+  ))
+  if (inherits(counts, "try-error")) {
+    counts <- try(read.csv(file, sep = "\t"), silent = TRUE)
+    if (inherits(counts, "try-error")) {
+      msg("FATAL: could not read abundance file")
+      return(NULL)
+    }  
+  }
+  
+  counts <- counts[!is.na(rownames(counts)) & rownames(counts) != "", , drop = FALSE]
+  if (!any(is.na(suppressWarnings(as.numeric(rownames(counts)))))) {
+    rownames(counts) <- paste0("P", 1:nrow(counts))
+  }
+  msg("Intial matrix size: ", nrow(counts), " x ", ncol(counts))
+  
+  pg.cols <- grep("^PG\\.", colnames(counts), value = TRUE)
+  if (length(pg.cols) > 0) msg("pg.cols = ", paste(pg.cols, collapse = "; "))
+
+  ## Columns required: PG.ProteinGroups; PG.Genes; PG.Quantity;
+  pg.idx <- grep("proteingroups|accession", pg.cols, ignore.case = TRUE)
+  genes.idx <- grep("\\.genes", pg.cols, ignore.case = TRUE)
+  if (length(pg.idx) == 0 & length(genes.idx) == 0) {
+    msg("FATAL: No protein groups, accession or genes found. Exiting")
+    return(NULL)
+  }
+  
+  protgr <- genes <- NULL
+  if (length(pg.idx) > 0) protgr <- counts[, pg.cols[pg.idx], drop = FALSE]
+  if (length(genes.idx) > 0) genes <- counts[, pg.cols[genes.idx], drop = FALSE] 
+
+  ## Remove contaminants
+  if (!is.null(protgr)) {
+    is.contam <- rowSums(sapply(protgr, grepl, pattern = "Cont_", ignore.case = TRUE)) > 0
+    if (any(is.contam)) {
+      msg("Identified ", sum(is.contam), " contaminants. Removing...")
+      counts <- counts[!is.contam, , drop = FALSE]
+      protgr <- protgr[!is.contam, , drop = FALSE]
+      if (!is.null(genes)) genes <- genes[!is.contam, , drop = FALSE]
+    }
+  }
+
+  ## Abundance data
+  quant.idx <- grep("\\.quantity", colnames(counts), ignore.case = TRUE)
+  if (length(quant.idx) == 0) {
+    msg("FATAL: No abundances data found. Exiting")
+    return(NULL)
+  }
+  msg(length(quant.idx), " protein abundances columns found...")
+  counts <- counts[, quant.idx, drop = FALSE]
+
+  ## Combine annot and abundance
+  counts <- cbind(protgr, genes, counts)
+  hh <- grep(".proteingroups|accession", colnames(counts), ignore.case = TRUE)
+  if (length(hh) > 0) {
+    rownames(counts) <- make.unique(counts[, hh[1]])
+    counts <- counts[, -hh[1], drop = FALSE]
+  }
+
+  ## Clean colnames
+  colnames(counts) <- gsub("^PG\\.|.PG.Quantity", "", colnames(counts))
+  colnames(counts) <- gsub("^X\\.[0-9]+|\\.{2,}", "", colnames(counts))
+  colnames(counts) <- gsub("\\.d$", "", colnames(counts))
+  colnames(counts) <- gsub("-", "_", gsub("\\.", "_", colnames(counts)))
+
+  msg("Completed. Final matrix size: ", nrow(counts), " x ", ncol(counts))
+  rm(pg.cols, protgr, genes); gc()
+
+  return(counts)
+
+}
+
+
 
 #' Read gene/probe annotation file
 #'
